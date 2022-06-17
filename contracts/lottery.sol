533dd65432bde163f48fdfbb5ec3e5d7a310df4e
Mayank <mayankghz225@gmail.com>
2022-06-17 19:39:15 +0530

    first comit

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Lottery {
-    address public manager;
-    constructor(){}
+contract Lottery { 
+    constructor(){
+        manager = msg.sender;
+      }
+
+        address public manager;
+        address payable[] public participants;
+        
+       
+
+        receive()  external payable{
+            require(msg.value==2 ether);
+            participants.push(payable(msg.sender));
+        }
+
+        function getBalance() public view returns(uint){
+            require(msg.sender==manager);
+            return address(this).balance;
+        }
+ 
+    }
+
+
 }
